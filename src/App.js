68924714180846bdad8f4a82601379c6b16ae204
gregorydaval345 <davalgregory69@gmail.com>
2022-02-12 18:57:30 -0500

    Add toggle logic for the toggle buttons

--- src/App.js
+++ src/App.js
@@ -2,33 +2,82 @@ import React, { useState, useEffect} from 'react';
 import { Switch } from 'antd';
 import axios from 'axios';
 import './App.css';
+import { Tab, Table } from 'semantic-ui-react';
+import _, { set } from 'lodash'
 import Currency from './components/Currency';
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
 
+  // Toggles:
+  const [usEnabled, setUsEnabled] = useState(false);
+  const [testModeEnabled, setTestModeEnabled] = useState(false);
+
+  // Button Sort Logic:
+  const [sortEnabled, setSort] = useState();
+
+
+  function fetchCoin() {
+    return axios
+    .get(
+      'https://api.moonpay.com/v3/currencies'
+    )
+    .then(res => {
+      let filterData = res.data;
+      if (usEnabled) {
+        console.log(usEnabled);
+        filterData = filterData.filter(coin => coin.isSupportedInUS)
+      }
+      if (testModeEnabled) {
+        filterData = filterData.filter(coin => coin.supportsTestMode)
+      }
+      if (sortEnabled) {
+        filterData = filterData.sort(coin => function(a, b)
+        {
+         var x = a[coin.name]; 
+         var y = b[coin.name];
+         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        }); // RESEARCH : sort list of object by key
+      }
+      setCoins(filterData)
+      console.log(res.data);
+    })
+    .catch(error => console.log(error));
+  }
+
   useEffect(() => {
-    axios
-      .get(
-        'https://api.moonpay.com/v3/currencies'
-      )
-      .then(res => {
-        setCoins(res.data)
-        console.log(res.data);
-      })
-      .catch(error => console.log(error));
-  }, []);
+    fetchCoin()
+  }, [usEnabled, testModeEnabled, sortEnabled]); //re-run// re-fetch
 
   const handleChange = e => {
     setSearch(e.target.value);
   };
 
-  // Filtering by name or symbol
+  // Filtering by name or symbol/code
   const filteredCoins = coins.filter(coin =>
     coin.name.toLowerCase().includes(search.toLowerCase())
     || coin.code.toLowerCase().includes(search.toLowerCase())
   );
+
+
+   function OnUsClickEvent(checked, event) {
+     setUsEnabled(checked)
+     //fetchCoin()
+  }
+
+  console.log({usEnabled, testModeEnabled})
+
+  function OnTestModeClickEvent(checked, event) {
+    setTestModeEnabled(checked)
+    //fetchCoin()
+  }
+
+  // Button Sort functions:
+
+  function onSortButtonByName() {
+    setSort(!sortEnabled)
+  }
 
   return (
     <div className='coin-app'>
@@ -44,10 +93,11 @@ function App() {
         </form>
         <div>
           <h4>Toggles:</h4>
-          <Switch size='large' checkedChildren='Non US' unCheckedChildren='US' />
+          <Switch onClick={OnUsClickEvent} size='large' checkedChildren='Non US' unCheckedChildren='US'/>
           <br />
-          <Switch size='large' checkedChildren='Non Test Mode' unCheckedChildren='Test Mode'/>
+          <Switch onClick={OnTestModeClickEvent} size='large' checkedChildren='Non Test Mode' unCheckedChildren='Test Mode'/>
         </div>
+        <button onClick={onSortButtonByName} >Sort By Name</button>
       </div>
       {filteredCoins.map(currency => {
         return (
@@ -64,14 +114,3 @@ function App() {
 }
 
 export default App;
-
-/*
-Pull data from api:
-
-- id
-- type
-- name
-- code
-
-
-*/
