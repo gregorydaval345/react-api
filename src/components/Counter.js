f002e6cd6b834d44fad0e844d3feb9172717248d
gregorydaval345 <davalgregory69@gmail.com>
2022-07-02 21:28:02 +0100

    Assignment

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      Count: {count}
+      <div>
+        <button onClick={() => setCount(count + 1)}>Smash Counter</button>
+      </div>
+    </div>
+  );
+}
